53142ef64b5bf6c8bacd65706ca41fd1b7b87e47
fred-fittywebdev <frederic-guerra_student2021@wilder.school>
2021-07-12 14:27:31 +0200

    Mise en place de la focntion édition des citations

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 const Article = ({article}) => {
+
+    const [isEditing, setIsEditing] = useState(false)
+    const [editedContent, setEditedContent] = useState('')
 
     const dateParser = (date) => {
         //let newDate = new Date(date).toLocaleDateString('fr-FR') -> Renvoie une date classique sous ce format, 07/07/2021
@@ -14,15 +18,38 @@ const Article = ({article}) => {
         return newDate
     }
 
+    const handleEdit = () => {
+
+        const data = {
+            author: article.author,
+            content: editedContent ? editedContent : article.content,
+            date: article.date,
+        }
+
+        axios.put('http://localhost:3003/Citations/' + article.id, data)
+        .then(() => {
+            setIsEditing(false)
+        })
+    }
+
     return (
-        <div className="article">
+        <div className="article"
+            style ={{background: isEditing ? "#cacaca" : "white"}}>
             <div className="card-header">
                 <h3> {article.author} </h3>
                 <em>Postée le: {dateParser(article.date)}</em>
             </div>
-            <p> {article.content} </p>
+            {isEditing ? (
+                <textarea onChange={(e) => setEditedContent(e.target.value)} autoFocus defaultValue={editedContent ? editedContent : article.content}></textarea>
+            ) : (
+                <p> {editedContent ? editedContent : article.content} </p>
+            ) }
             <div className="btn-container">
-                <button>Editer</button>
+            {isEditing ? [
+                <button onClick={handleEdit}>Valider</button>
+            ] : (
+                <button onClick={() => setIsEditing(true)}>Editer</button>
+            )}
                 <button>Effacer</button>
             </div>
         </div>
